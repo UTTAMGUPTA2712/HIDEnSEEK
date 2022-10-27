901585fe0b0b9b572e3ced79dc1b918dccd9a053
UTTAM GUPTA <71481012+UTTAMGUPTA2712@users.noreply.github.com>
2022-10-27 22:22:14 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,109 @@
+
+const prize=new Audio('gamepoint.wav');
+const nulll=new Audio('lose.wav');
+const endd=new Audio('totallose.wav');
+const winn=new Audio('totalwin.wav');
+const themee=new Audio('theme.mp3');
+
+themee.play();
+themee.setAttribute('loop', 'loop');
+function size(n,m){  
+    var moves=0;
+    var count=0;
+    var arr=new Array(n*n);
+    arr.fill(null);
+    var r;
+    for(var i=0;i<n;i++){
+        r=Math.floor(Math.random()*(n*n));
+        if(arr[r]==null)
+            arr[r]='🏆';
+        else
+            i--;
+    }
+    for(var i=0;i<(n-1)/2;i++){
+        r=Math.floor(Math.random()*(n*n));
+        if(arr[r]==null)
+            arr[r]='💣';
+        else
+            i--;
+    }
+    console.log(arr);                  /*uncomment this line to check the cheat codes on console*/
+    document.getElementById("choicebox").style["display"] = "none"; 
+    document.getElementById("back").style["display"] = "block"; 
+    document.getElementById("back").style["visibility"] = "visible";
+    document.getElementById("guess").style["display"] = "block"; 
+    document.getElementById("guess").style["visibility"] = "visible";
+    document.getElementById("contai").style["display"] = "block"; 
+    var container = document.getElementById("contai");
+    for(var i=0;i<n;i++){
+            var cell2=document.createElement("div");
+        for(var j=0;j<m;j++){
+            var cell = document.createElement("div");
+            cell.innerHTML =String.fromCharCode(i+65)+j;
+            cell.className="col";
+            cell.id=String.fromCharCode(i+65)+j;
+            cell2.appendChild(cell);
+        }
+        container.appendChild(cell2);
+    }
+    document.getElementById("guessbtn").addEventListener("click",guessed,false);
+    window.addEventListener('keyup', function(event) {
+        if (event.key === 'Enter') {
+        guessed();
+        }
+    });
+function guessed(){
+    document.getElementById("error").innerHTML="";
+    moves++;
+    var choosed=document.getElementById("inputfield").value;
+    var num=(choosed.charCodeAt(0)-65)*n+choosed.charCodeAt(1)-48;
+    if(document.getElementById(choosed)==null){
+        document.getElementById("error").innerHTML="!!!!!! No Such Option !!!!!";
+        document.getElementById("inputfield").value="";
+        return;
+    }else if(arr[num]==-1){
+        document.getElementById("error").innerHTML="!!!!! Already selected !!!!!";
+        document.getElementById("inputfield").value="";
+        return;
+    }
+    console.log(choosed+" moves-"+moves+" count"+count);    /*uncomment this line to check the cheat codes on console*/
+    if(arr[num]==null){
+        nulll.play();
+        arr[num]=-1;
+        document.getElementById(choosed).style.color="transparent";
+        document.getElementById(choosed).style.backgroundImage= "url('none.gif')";
+    }
+    else if(arr[num]=='🏆'){
+        prize.play();
+        document.getElementById(choosed).style.color="transparent";
+        document.getElementById(choosed).style.backgroundImage= "url('diamond.gif')";
+        count++;
+        arr[num]=-1;
+        console.log(count);
+        if(count==n){
+            setTimeout(youwin,500);
+        }
+    }
+    else if(arr[num]=='💣'){
+        themee.pause();
+        endd.play();
+        document.getElementById(choosed).style.color="transparent";
+        document.getElementById(choosed).style.backgroundImage= "url('bomb.gif')";
+        setTimeout(youlose,1000);
+    }
+    document.getElementById("inputfield").value="";
+    function youwin(){
+        winn.play();
+        document.getElementById("scorer").innerHTML="Your accuracy is "+(count/moves*100)+"%"; 
+        document.getElementById("youwon").style["display"] = "block"; 
+        document.getElementById("youwon").style["visibility"] = "visible"; 
+    }
+    function youlose(){
+        document.getElementById("gameover").style["display"] = "block"; 
+        document.getElementById("gameover").style["visibility"] = "visible"; 
+    }
+}
+}
+function back(){
+    window.location.href=window.location.href;
+}
